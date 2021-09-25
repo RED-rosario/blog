c6b373e3e21c76813962fe3440de8c398ab10f05
RED-rosario <marciarosario2021@gmail.com>
2021-09-24 23:22:26 -0300

    article_params que filtre los params

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,17 +7,22 @@
     end  
 
     def new
-      @article = Article.new
+      @article = Article.new 
     end
   
     def create
-      @article = Article.new(title: "...", body: "...")
-        if @article.save
+      @article = Article.new(article_params)
+        
+       if @article.save
         redirect_to @article
-      else
+       else
         render :new
-      end
+       end
     end
+    private
+       def article_params
+         params.require(:article).permit(:title, :body)
+       end
   end
 
 
